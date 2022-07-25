ca238e71a91b639d07d3b1215957ac33037ece5a
Artem Anosov <aanosov@rtech.ru>
2022-07-25 22:10:33 +0300

    init commit

---
+++ src/main/java/org/thundertech/csv/Csv.java
@@ -0,0 +1,46 @@
+package org.thundertech.csv;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
+public class Csv {
+
+    private static void divideCsv(String csvFilePath, String quote, String header, int maxSizeBytes,
+                                  String pathToSave) throws IOException {
+
+        String newLine = System.lineSeparator();
+        String containsTwoQuoteSymbol = "(.?|.+)" + quote + "(.?|.+)" + quote + "(.?|.+)";
+        Path filePath = Paths.get("");
+        int fileCounter = 0;
+        var csvLines = Files.lines(Paths.get(csvFilePath)).skip(1).toList();
+        int quoteCounter = 0;
+        boolean isOverSized = true;
+
+        for(String line : csvLines) {
+            if(isOverSized) {
+                fileCounter++;
+                filePath = Paths.get(pathToSave + "cuba_session_" + fileCounter + ".csv");
+                Files.write(filePath, (header + newLine).getBytes());
+                isOverSized = false;
+            }
+
+            Files.write(filePath, (line  + newLine).getBytes(),
+                    StandardOpenOption.APPEND);
+
+            if(line.contains(quote)) {
+                quoteCounter++;
+                if(quoteCounter == 2 || line.matches(containsTwoQuoteSymbol)) {
+                    isOverSized = Files.size(filePath) > maxSizeBytes;
+                    quoteCounter = 0;
+                }
+            } else {
+                if(quoteCounter == 0) {
+                    isOverSized = Files.size(filePath) > maxSizeBytes;
+                }
+            }
+        }
+    }
+}
